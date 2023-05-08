1e3115e9018bc82cdab38721f37dc907815805f0
Damzgod <oluwadamilola.olorunda@binghamuni.edu.ng>
2023-05-08 20:22:31 +0430

    Initial commit

---
+++ phy.py
@@ -0,0 +1,13 @@
+
+class Physics:
+
+    def mass(self, x, y):
+        return x / y
+    def force(self, x, y):
+        return x * y
+    def velocity(self, x, y):
+        return x / y
+    def distance(self, x, y):
+        return x * y
+    def speed(self,x ,y):
+        return x / y
